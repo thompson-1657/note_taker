a2ac58ab157daf580d91ec7b61e9420ae563e414
Rachel Thompson <thompson.1657@gmail.com>
2021-01-29 10:23:39 -0600

    get req for /api/notes

---
+++ server.js
@@ -0,0 +1,38 @@
+const path = require('path')
+const fs = require('fs')
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
+
+//used to consume json data in routes 
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+
+
+// HTML ROUTES
+// app.get('/notes', (req, res) => {
+//     req.sendFile(path.join(__dirname + '/public/notes.html'))
+// })
+
+// app.get('*', (req, res) => {
+//     req.sendFile(path.join(__dirname + '/public/index.html'))
+// })
+
+// API ROUTES
+fs.readFile("db/db.json", "utf8", (err, data) => {
+    if (err) throw err
+    let notes = JSON.parse(data)
+
+    app.get('/api/notes', (req, res) => {
+        console.log('notes');
+        res.json(notes)
+    })
+
+})
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server listening on http://localhost:${PORT}`);
+})
